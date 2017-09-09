fd058698839944b4338f774d614133bbd09993e9
randomnf <30911241+randomnf@users.noreply.github.com>
2017-09-09 17:52:18 +0300

    завершенная разметка сетки, осталось подштриховать декторатив

---
+++ js/form-toggle.js
@@ -0,0 +1,7 @@
+﻿var btn = document.querySelector(".form-show-btn");
+var form = document.querySelector(".find-hotel-form");
+
+btn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  form.classList.toggle("find-hotel-form-hidden");
+});
